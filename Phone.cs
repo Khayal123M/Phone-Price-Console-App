471245a6c1e77dae8998b67a7b2752f9bd69b1ac
Khayal123M <132755333+Khayal123M@users.noreply.github.com>
2023-05-11 19:12:19 +0300

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Price_Console_App
+{
+    public class Phone
+    {
+        // Parameters
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone_Brand { get; set; }
+        public string Operating_System { get; set; }
+        public int RAM_Size { get; set; }
+        public int Phone_Price { get; set; }
+        public int Balance { get; set; }
+
+        // Methods
+
+        public void Personel_Info()
+        {
+
+            Console.WriteLine("Name = " + Name);
+            Console.WriteLine("Surname = " + Surname);
+
+
+        }
+
+        public void Phone_Info()
+        {
+            Console.WriteLine("Operating System = " + Operating_System);
+            Console.WriteLine("Phone Brand = " + Phone_Brand);
+            Console.WriteLine("Ram Size = " + RAM_Size);
+            Console.WriteLine("Phone Price = " + Phone_Price);
+
+        }
+
+        public void Purchasability()
+        {
+            if(Balance >= Phone_Price)
+            {
+                Console.WriteLine("You have sufficient balance. You can purchase the phone.");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough balance to purchase this phone.");
+            }
+        }
+
+
+    }
+
+
+    
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Phone_Price_Console_App;
+
+namespace MainPage
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Phone phone = new Phone();
+
+            phone.Name = "Khayal";
+            phone.Surname = "Mamedov";
+            phone.Phone_Brand = "Samsung";
+            phone.Phone_Price = 250;
+            phone.Operating_System = "Android";
+            phone.RAM_Size = 4;
+
+
+            while (true)
+            {
+                Console.WriteLine("Choise");
+                int Choise = Convert.ToInt32(Console.ReadLine());
+                if (Choise == 1)
+                {
+                    phone.Personel_Info();
+                }
+                else if (Choise == 2)
+                {
+                    phone.Phone_Info();
+                }
+                else if (Choise == 3)
+                {
+                    Console.WriteLine("Please enter your balance");
+                    phone.Balance = Convert.ToInt32(Console.ReadLine());
+                    phone.Purchasability();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choise");
+                }
+
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+}
